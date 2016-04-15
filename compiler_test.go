2f0c97af0db27e5264ac061a97aece3c9d291e08
= <hazbo@gmx.com>
2016-04-15 11:47:51 +0100

    Implemented TestParser()

--- compiler_test.go
+++ compiler_test.go
@@ -5,49 +5,87 @@ import (
 	"testing"
 )
 
-const input = "(add 10 (subtract 10 6))"
+const testInput = "(add 10 (subtract 10 6))"
+
+var testTokens = []token{
+	token{
+		kind:  "paren",
+		value: "(",
+	},
+	token{
+		kind:  "name",
+		value: "add",
+	},
+	token{
+		kind:  "number",
+		value: "10",
+	},
+	token{
+		kind:  "paren",
+		value: "(",
+	},
+	token{
+		kind:  "name",
+		value: "subtract",
+	},
+	token{
+		kind:  "number",
+		value: "10",
+	},
+	token{
+		kind:  "number",
+		value: "6",
+	},
+	token{
+		kind:  "paren",
+		value: ")",
+	},
+	token{
+		kind:  "paren",
+		value: ")",
+	},
+}
+
+var testAst = ast{
+	kind: "Program",
+	body: []node{
+		node{
+			kind: "CallExpression",
+			name: "add",
+			params: []node{
+				node{
+					kind:  "NumberLiteral",
+					value: "10",
+				},
+				node{
+					kind: "CallExpression",
+					name: "subtract",
+					params: []node{
+						node{
+							kind:  "NumberLiteral",
+							value: "10",
+						},
+						node{
+							kind:  "NumberLiteral",
+							value: "6",
+						},
+					},
+				},
+			},
+		},
+	},
+}
 
 func TestTokenizer(t *testing.T) {
-	expected := []token{
-		token{
-			kind:  "paren",
-			value: "(",
-		},
-		token{
-			kind:  "name",
-			value: "add",
-		},
-		token{
-			kind:  "number",
-			value: "10",
-		},
-		token{
-			kind:  "paren",
-			value: "(",
-		},
-		token{
-			kind:  "name",
-			value: "subtract",
-		},
-		token{
-			kind:  "number",
-			value: "10",
-		},
-		token{
-			kind:  "number",
-			value: "6",
-		},
-		token{
-			kind:  "paren",
-			value: ")",
-		},
-		token{
-			kind:  "paren",
-			value: ")",
-		},
+	result := tokenizer(testInput)
+	if !reflect.DeepEqual(result, testTokens) {
+		t.Error("\nExpected:", testTokens, "\nGot:", result)
 	}
-	result := tokenizer(input)
-	if !reflect.DeepEqual(result, expected) {
-		t.Error("\nExpected:", expected, "\nGot:", result)
+}
+
+func TestParser(t *testing.T) {
+	result := parser(testTokens)
+	if !reflect.DeepEqual(result, testAst) {
+		t.Error("\nExpected:", testAst, "\nGot:", result)
 	}
 }
